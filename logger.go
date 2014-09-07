88edf9efceb65038047c8390bc1a61db2fa4576e
Unknwon <joe2010xtmf@163.com>
2014-09-07 09:38:46 -0400

    Add Context.RemoadAddr()

--- logger.go
+++ logger.go
@@ -34,14 +34,7 @@ func Logger() Handler {
 	return func(res http.ResponseWriter, req *http.Request, c *Context, log *log.Logger) {
 		start := time.Now()
 
-		addr := req.Header.Get("X-Real-IP")
-		if addr == "" {
-			addr = req.Header.Get("X-Forwarded-For")
-			if addr == "" {
-				addr = req.RemoteAddr
-			}
-		}
-		log.Printf("Started %s %s for %s", req.Method, req.URL.Path, addr)
+		log.Printf("Started %s %s for %s", req.Method, req.URL.Path, c.RemoteAddr())
 
 		rw := res.(ResponseWriter)
 		c.Next()
