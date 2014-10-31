35b2c8878034032e31604ce617f9f3a535b37b4f
Unknwon <joe2010xtmf@163.com>
2014-10-31 19:26:39 -0400

    panic ui

--- recovery.go
+++ recovery.go
@@ -47,6 +47,11 @@ pre {
 	padding: 20px;
 	border: 2px solid #2b3848;
 	background-color: #ffffff;
+	white-space: pre-wrap;       /* css-3 */
+	white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+	white-space: -pre-wrap;      /* Opera 4-6 */
+	white-space: -o-pre-wrap;    /* Opera 7 */
+	word-wrap: break-word;       /* Internet Explorer 5.5+ */
 }
 </style>
 </head><body>
