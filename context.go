0256aea6323936f1496a4e09f25e2c03a29920a1
Meaglith Ma <genedna@gmail.com>
2016-08-30 21:12:01 +0000

    Add Content-Length in ServeFile when Content-Encoding is not null

--- context.go
+++ context.go
@@ -499,6 +499,15 @@ func (ctx *Context) ServeFile(file string, names ...string) {
 	ctx.Resp.Header().Set("Expires", "0")
 	ctx.Resp.Header().Set("Cache-Control", "must-revalidate")
 	ctx.Resp.Header().Set("Pragma", "public")
+
+	if ctx.Resp.Header().Get("Content-Encoding") != "" {
+		data, _ := os.Open(name)
+		header := make([]byte, 512)
+		data.Read(header)
+		stat, _ := data.Stat()
+		ctx.Resp.Header().Set("Content-Length", strconv.FormatInt(stat.Size(), 10))
+	}
+
 	http.ServeFile(ctx.Resp, ctx.Req.Request, file)
 }
 
