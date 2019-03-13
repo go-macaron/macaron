4df59ae8a50bd2e82ed915ff1ce764b1bb128a9d
Unknwon <u@gogs.io>
2019-03-13 17:40:39 -0400

    context: fix method comments
    [CI SKIP]

--- context.go
+++ context.go
@@ -162,12 +162,12 @@ func (ctx *Context) renderHTML(status int, setName, tplName string, data ...inte
 	}
 }
 
-// HTML calls Render.HTML but allows less arguments.
+// HTML renders the HTML with default template set.
 func (ctx *Context) HTML(status int, name string, data ...interface{}) {
 	ctx.renderHTML(status, DEFAULT_TPL_SET_NAME, name, data...)
 }
 
-// HTMLSet calls Render.HTMLSet but allows less arguments.
+// HTMLSet renders the HTML with given template set name.
 func (ctx *Context) HTMLSet(status int, setName, tplName string, data ...interface{}) {
 	ctx.renderHTML(status, setName, tplName, data...)
 }
